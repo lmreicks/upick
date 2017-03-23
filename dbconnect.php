ca5eaffb02d82d65e21a0dbf39d06a0902071c02
Lexi Reicks <lmreicks@iastate.edu>
2017-03-23 13:18:04 -0500

    Delete dbconnect.php

--- dbconnect.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('DBHOST','192.254.190.129');
-define('DBUSER','rreicks1_lexi');
-define('DBPASS','6w)Z#eP4-hZQ');
-define('DBNAME','rreicks1_UPICK');
-
-$db = new mysqli(DBHOST, DBUSER, DBPASS, DBNAME);
-?>
