68980c7187f969c9f98dbf7ce90166fb6ace2a38
https://github.com/gould-ann/hackISU.git <lmreicks@iastate.edu>
2017-03-31 10:21:23 -0500

    added more fields to database and updated movies.php to fill those fields

--- server/rest-api/movies.php
+++ server/rest-api/movies.php
@@ -110,30 +110,66 @@ $app->get('/movies/{id}/more', function ($request, $response, $args) {
     $query = "SELECT * FROM movies WHERE id =". $id;
     $movie = $db->query($query)->fetch_assoc();
     $releaseDate[] = explode('-', $movie['release_date']);
+    //if we don't have an imdb key, get it.'
+    if ($movie['imdb_id'] == NULL) {
+        $getIMDB = "https://api.themoviedb.org/3/movie/" . $id . "?api_key=733865115819c8da6e8cc41c46684ed8&language=en-US";
+        $imdbresponse = curl($getIMDB);
+        $imdbresponseArray = json_decode($imdbresponse, true);
+        $imdbId = $imdbresponseArray['imdb_id'];
+        $imdbkey = "'" . $imdbresponseArray['imdb_id'] . "'";
+        $query = "UPDATE movies SET `imdb_id`= " . $imdbkey;
+        $db->query($query);
+    }
+
     if($movie['hasMoreInfo'] == 1) {
         echo json_encode($movie);
     } else {
         // We haven't fetched additional data yet, lets do it now
         $netflixApi = "http://netflixroulette.net/api/api.php?title=" . $movie['title'] . "&year=" . $releaseDate[0][0];
         $getTrailerUrl = "https://api.themoviedb.org/3/movie/" . $id . "/videos?api_key=733865115819c8da6e8cc41c46684ed8&language=en-US";
+        $openMovieRequest = "http://www.omdbapi.com/?i=" . $imdbId;
+        $recommendedRequest = "https://api.themoviedb.org/3/movie/" . $id . "/recommendations?api_key=733865115819c8da6e8cc41c46684ed8&language=en-US";
+
         $netflixresponse = curl($netflixApi);
         $trailerResponse = curl($getTrailerUrl);
+        $openMovieResponse = curl($openMovieRequest);
+        $recommendedResponse = curl($recommendedRequest);
 
         $trailerResponseArray = json_decode($trailerResponse, true);
         $netflixresponseArray = json_decode($netflixresponse, true);
+        $openMovieResponseArray = json_decode($openMovieResponse, true);
+        $recommendedResponseArray = json_decode($recommendedResponse, true);
+
+        echo json_encode($openMovieResponseArray);
+
         $trailerKey =  "'" . $trailerResponseArray['results'][0]['key'] . "'";
+
         $Netid = "NULL";
         $cast = "NULL";
         $director = "NULL";
         $actors = "NULL";
+        $recommended = "NULL";
+
+        $parental_rating = "'" . $openMovieResponseArray['Rated'] . "'";
+        $ratings = $openMovieResponseArray['Ratings']['Source'];
+        $rottentomatoes = "'" . $ratings['Rotten Tomatoes']['Value'] . "'";
+        $imdbrating = "'" . $openMovieResponseArray['imdbRating'] . "'";
+        $plot = "'" . $openMovieResponseArray['Plot'] . "'";
+
+        //come back to this, should we just return the title, id, poster path?
+        //$recommended = "'" . $recommendedResponseArray['']
+
         if ($netflixresponseArray['errorcode'] == 404) {
             // Movie is not on netflix
+            // get cast/director from imdb
+            $cast = "'" . $openMovieResponseArray['Actors'] . "'";
+            $director = "'" . $openMovieResponseArray['Director'] . "'";
         } else {
             $Netid = "'" . $netflixresponseArray["show_id"] . "'";
             $cast = "'" . $netflixresponseArray['show_cast'] . "'";
             $director = "'" . $netflixresponseArray['director'] . "'";
         }
-        $query = "UPDATE movies SET `hasMoreInfo`= 1, `netflix_id` = " . $Netid . ", `actors` = " . $cast . ", `trailer_url` = " . $trailerKey . ", `director` = " . $director . " WHERE id=" . $id;
+        $query = "UPDATE movies SET `hasMoreInfo`= 1, `netflix_id` = " . $Netid . ", `actors` = " . $cast . ", `trailer_url` = " . $trailerKey . ", `director` = " . $director . ", `imdb_rating` = " . $imdbrating . ", `parental_rating` = " . $parental_rating . ", `rotten_tomatoes` = " . $rottentomatoes . ", `recommended` = " . $recommended . " WHERE id=" . $id;
         $db->query($query);
         $movie = $db->query("SELECT * FROM movies WHERE id =". $id)->fetch_assoc();
         echo json_encode($movie);
