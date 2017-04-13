1a029234430bf27095fd83912950384c6b7c885b
https://github.com/gould-ann/hackISU.git <lmreicks@iastate.edu>
2017-04-13 13:11:25 -0500

    dropdown styling and functionality

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ElementRef, NgZone } from '@angular/core';
 import { Router } from '@angular/router';
 import { Movie } from './models/movie.model';
 import { FormControl } from '@angular/forms';
@@ -14,23 +14,60 @@ import { MovieService } from './services/movie.service';
 export class AppComponent {
   name = 'UPick';
   @Input('query') query: string;
+  lastScrollTop: number = 0;
+  direction: string = "";
+  headerVisible: boolean = true;
+  dropVisible: boolean = false;
   navVisible: boolean = true;
   searchVisible: boolean = true;
   searchItems: any;
   term = new FormControl();
 
-  constructor(public MovService: MovieService, public router: Router) {
-    this.term.valueChanges.debounceTime(400)
-                          .distinctUntilChanged()
-                          .flatMap(term => this.MovService.movieSearch(term))
-                          .subscribe(res => this.searchItems = res);
-  }
+  constructor(public MovService: MovieService, public router: Router, private lc: NgZone) {
+    this.term.valueChanges
+      .debounceTime(400)
+      .distinctUntilChanged()
+      .flatMap(term => this.MovService.movieSearch(term))
+      .subscribe(res => this.searchItems = res);
+  
 
+  window.onscroll = () => {
+        let st = window.pageYOffset;
+        let dir = '';
+        if (st > this.lastScrollTop) {
+            dir = "down";
+        } else {
+            dir = "up";
+        }
+        this.lastScrollTop = st;
+        lc.run(() => {
+          this.direction = dir;
+        });
+      if (dir == "down") {
+        this.headerVisible = false;
+      }
+      else {
+        this.headerVisible = true;
+        //top++;
+      }
+    };
+  }
   getRandom() {
     let vm = this;
     this.MovService.getRandom().then(function (res) {
       vm.router.navigate(['movie', res.id]);
     });
+  }
+
+  selectItem(event:any) {
+    if (this.dropVisible) {
+      if(event.key == "ArrowDown") {
+        
+      }
+      if (event.key == "ArrowUp") {
+
+      }
+    }
   }
 }
 
